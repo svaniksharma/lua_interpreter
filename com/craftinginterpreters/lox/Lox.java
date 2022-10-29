8a6c5ba54e5bb73c249598fae23bee63db32b599
gh0std3v <ssharma10393@gmail.com>
2022-10-29 16:49:57 -0500

    Completed Resolver chapter

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -51,6 +51,9 @@ public class Lox {
         List<Stmt> statements = parser.parse();
         // Stop if there was a syntax error.
         if (hadError) return;
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+        if (hadError) return;
         interpreter.interpret(statements);
     }
 
